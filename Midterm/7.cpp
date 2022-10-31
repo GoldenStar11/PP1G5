2797c9a16179b385c1562af3bd6a6b5c84b048b0
Yerkebulan Bekmukhayev <bekmukhayev@gmail.com>
2022-10-31 21:08:09 +0600

    Midterm

--- Midterm/7.cpp
+++ Midterm/7.cpp
@@ -1,25 +1,70 @@
-#include <iostream>
+#include<iostream>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
-    int n, cnt = 0, max = -1;
+    int n;
     cin >> n;
     int a[n];
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n;i++){
         cin >> a[i];
     }
-     //1 1 1 2 3 4 5 6 7 8 9
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(a[i] == a[j]){
-                cnt++;
+
+    sort(a, a +n); // sorted array 
+    int cnt = 0;
+    for(int i = 0; i < n;i++){
+        if(a[i] != a[i+1]){
+            cnt++;
+        }
+    }
+
+    // count уникальных элементов
+
+    int no_dup[cnt][2];
+    // массив no_dup[cnt][0] это уникальные элементы ,
+    // no_dup[cnt][1] -счетчик  сколько их в ориг массиве 
+    /*1 1 1 2 2 3 3 3 3
+        1 3     1 0
+        2 2     2 0
+        3 4     3 0
+    */
+    int k = 0;
+
+    for(int i = 0; i < n;i++){
+        if(a[i] != a[i+1]){
+            no_dup[k][0] = a[i];
+            no_dup[k][1] = 0;
+            // cnt++;
+            k++;
+        }
+    }
+    /* 1 1 1 2 2 3 3 3 3 
+        1 3
+        2 2
+        3 4
+    */
+    for(int j = 0;j < n;j++){
+        for(int i = 0; i < cnt;i++){
+            if(no_dup[i][0] == a[j]){
+                no_dup[i][1]++;
             }
         }
-        if(max < cnt - 1){
-             max = cnt;
-        }
-        cnt = -1;
     }
-    cout << max;
+
+    int max1 = INT_MIN;
+    for(int i = 0; i < cnt;i++){
+        if( max1 < no_dup[i][1]){
+            max1 = no_dup[i][1]; // 4
+        }
+
+    }
+
+    for(int i = cnt-1; i >= 0;i--){
+        if( max1 == no_dup[i][1]){
+            cout <<  no_dup[i][0] << " ";
+        }
+
+    }
+    return 0;
 }
