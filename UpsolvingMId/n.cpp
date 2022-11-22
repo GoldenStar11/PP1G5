190868c59e7c554f5726b7f978af336d1db9f767
Yerkebulan Bekmukhayev <bekmukhayev@gmail.com>
2022-11-22 20:08:11 +0600

    Update n.cpp

--- UpsolvingMId/n.cpp
+++ UpsolvingMId/n.cpp
@@ -11,12 +11,14 @@ int main(){
     }
     for(int i = n-1; i > n/2 - 1; i--){
         int reversed_number = 0;
-        while(a[i] != 0) {
-            int remainder = a[i] % 10;
-            reversed_number = reversed_number * 10 + remainder;
-            a[i] /= 10;
+        while(a[i] != 0) {/*
+        2910
+        */
+            int remainder = a[i] % 10;  // 0 1 9 2
+            reversed_number = reversed_number * 10 + remainder; //0  1 19 190 + 2 = 192
+            a[i] /= 10; //91 9 2 0
         }
-        a[i] = reversed_number;
+        a[i] = reversed_number; // 19
     }
     for(int i = 0; i < n; i++){
         if(a[i] != a[n-1-i]){
