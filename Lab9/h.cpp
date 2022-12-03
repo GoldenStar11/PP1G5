f166421a89af889950c482c130f4c919645aeb59
Yerkebulan Bekmukhayev <bekmukhayev@gmail.com>
2022-12-03 19:48:41 +0600

    Lab 9

---
+++ Lab9/h.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    map <string, int> mp;
+    for(int i = 1; i <= n; i++){
+        string s;
+        cin >> s;
+        if(mp[s]) continue;
+        mp[s] = i;
+    }
+    map <string, int> :: iterator it;
+    for(it = mp.begin(); it != mp.end(); it++){
+        cout << it -> first << ' ' << it -> second << endl;
+    } 
+}
