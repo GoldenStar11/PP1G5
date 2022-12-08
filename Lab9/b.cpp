d3203219e20393c744db939f1d03493a34620d92
Yerkebulan Bekmukhayev <bekmukhayev@gmail.com>
2022-12-08 19:55:25 +0600

    .

--- Lab9/b.cpp
+++ Lab9/b.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    int n;
+    /*int n;
     cin >> n;
     vector <int> v1;
     vector <int> v2;
@@ -21,5 +21,22 @@ int main(){
     }
     for(int i = 0; i < v2.size(); i++){
         cout << v2[i] << ' ';
+    }*/
+    int n;
+    cin >> n;
+    map <int,string> mp;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        if(a % 2 == 0) mp[a] = "Even";
+        else mp[a] = "Odd";
+    }
+    map <int, string> :: reverse_iterator it;
+    for(it = mp.rbegin(); it != mp.rend(); it++){
+        if(it->second == "Even") cout << it->first << ' ';
+    }
+    map<int,string> :: iterator i;
+    for(i = mp.begin(); i != mp.end(); i++){
+        if(i->second == "Odd") cout << i->first << ' ';
     }
 }
