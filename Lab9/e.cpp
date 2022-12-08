f1f697a839b081e785a1585a74b66e439e5e4f1c
Yerkebulan Bekmukhayev <bekmukhayev@gmail.com>
2022-12-08 19:42:01 +0600

    .

--- Lab9/e.cpp
+++ Lab9/e.cpp
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-int main(){
-    map <string, int> mp;
+int main()
+{
     int n;
     cin >> n;
+    map <string, int> mp;
     for(int i = 0; i < n; i++){
-        string x;
-        cin >> x;
-        mp[x]++;
-    }// 01
-    int cnt = 0;
-    map <string,int> :: iterator it;
-    for(it = mp.begin(); it != mp.end(); it++){
-        if(it -> second == 3) cnt++;
+        string s;
+        cin >> s;
+        mp[s]++;
     }
-    cout << cnt;
-}
+    /*
+    number  counter
+    +700007 3
+    +707070 2
+    */
+   int cnt = 0;
+   map<string,int>::iterator it;
+   for(it = mp.begin(); it != mp.end(); it++){
+        if(it->second == 3){
+            cnt++;
+        }
+   }
+   cout << cnt;
+} 
---
+++ Lab9/m.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    queue <string> q;
+    int n;
+    cin >> n;
+    for(int i = 0;i < n; i++){
+        int a;
+        cin >> a;
+        if(a == 2) q.pop();
+        else{
+            string s;
+            cin>>s;
+            q.push(s);
+        }
+        if(!q.empty())  cout << q.front() <<endl;
+        else    cout<<"queue is empty"<<endl;
+    }
+}
